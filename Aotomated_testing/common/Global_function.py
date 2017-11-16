bc1681ec084559aea3c2f93f02489ff48f1f120c
270466585 <270466585@qq.com>
2017-11-16 15:20:50 +0800

    Gyl_automationV1.0.0

---
+++ Aotomated_testing/common/Global_function.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+from Base_page.LoginPage import Loginpage
+from common import read_Data
+import time as t
+
+'''
+该页面和内容方法适合整个自动化
+'''
+login_url = "xxxxxxx"
+
+
+def login(self):
+    '''
+    
+    :param self: 登陆
+    :return: 
+    '''
+    self.LOGIN=Loginpage(self.driver)
+    self.LOGIN.input_username(read_Data.getExcel1(5, 0))
+    self.LOGIN.input_password(read_Data.getExcel1(5, 1))
+    self.LOGIN.click_submit()
+    t.sleep(1)
